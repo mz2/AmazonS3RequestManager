1df9d2e7ce0bdd975d4c5e5ba05a547b07d18e1b
Matias Piipari <matias.piipari@gmail.com>
2017-05-22 20:14:45 +0100

    Adds Cartfile for dependencies and an xcodeproj + scheme for building.

---
+++ AmazonS3RequestManager/AmazonS3RequestManager/AmazonS3RequestManager.h
@@ -0,0 +1,20 @@
+//
+//  AmazonS3RequestManager.h
+//  AmazonS3RequestManager
+//
+//  Created by Matias Piipari on 22/05/2017.
+//  Copyright © 2017 Sashimiapp.com. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+#import "AmazonS3SignatureHelpers.h"
+
+//! Project version number for AmazonS3RequestManager.
+FOUNDATION_EXPORT double AmazonS3RequestManagerVersionNumber;
+
+//! Project version string for AmazonS3RequestManager.
+FOUNDATION_EXPORT const unsigned char AmazonS3RequestManagerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AmazonS3RequestManager/PublicHeader.h>
+
